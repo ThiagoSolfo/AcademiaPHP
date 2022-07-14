df0cb360216fe76d2e000f0bba77b73275e4c5c2
Thiago Solfo <61445366+ThiagoSolfo@users.noreply.github.com>
2022-07-14 11:26:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -11,33 +11,6 @@
  </head>
 
  <body>
-<<<<<<< HEAD
-=======
-   <?php
-    // Nome do servidor
-    $servername = "localhost";
-    // Nome do usuário do servidor
-    $username = "root";
-    // Senha do usuário do servidor
-    $password = "password";
-    // Nome do banco de dados à ser usado
-    $bd = "fitness";
-
-    // Cria a conexão
-    $conn = new mysqli($servername, $username, $password, $bd);
-
-    // Checa a conexão
-    if ($conn->connect_error) {
-      die("Conexão falha: " . $conn->connect_error);
-    } else {
-      echo "<p>Conectado com sucesso!</p>";
-    }
-    ?>
-
-   <h1>Academia Fitness</h1>
-
->>>>>>> parent of ef1c35b (Update index.php)
-
    <?php include('config.php'); ?>
    <marquee behavior="alternate" direction="down" width="100%" height="100%">
      <marquee behavior="alternate" direction="left" width="100%">
